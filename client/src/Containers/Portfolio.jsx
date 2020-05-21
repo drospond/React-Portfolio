3022c4a3a34f9342b9e5afc30734761bfbf78919
drospond <david.rospond@gmail.com>
2020-05-21 18:28:36 -0400

    updated and changed projects

--- client/src/Containers/Portfolio.jsx
+++ client/src/Containers/Portfolio.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Project from "../Components/Project/Project";
 import CodeQuiz from "../images/CodeQuiz.PNG";
-import Burger from "../images/EatDaBuergerScreenShot.PNG";
+// import Burger from "../images/EatDaBuergerScreenShot.PNG";
 import NoteTaker from "../images/NoteTaker.PNG";
 import springBreakers from "../images/SpringBreakers.PNG";
 import steamapp from "../images/steamapp.PNG";
 import WeatherDashboard from "../images/WeatherDashboard.PNG";
+import friendyfire from "../images/friendlyfire.PNG";
 
 class Portfolio extends Component {
   render() {
@@ -17,16 +18,16 @@ class Portfolio extends Component {
           </div>
         </div>
         <div className="row d-flex justify-content-center">
-          <Project title="Spring Breakers" imageSource={springBreakers} repoLink="https://github.com/drospond/springBreakers/" deployedLink="https://drospond.github.io/springBreakers/"/>
+          <Project title="Steam Games Compare" imageSource={steamapp} repoLink="https://github.com/drospond/project-2" deployedLink="https://steam-comparison.herokuapp.com/"/>
+          <Project title="Friendly Fire" imageSource={friendyfire} repoLink="https://github.com/drospond/FriendlyFire" deployedLink="https://secure-tor-59417.herokuapp.com/"/>
+        </div>
+        <div className="row d-flex justify-content-center">
+          <Project title="Code Quiz" imageSource={CodeQuiz} repoLink="https://github.com/drospond/JavaScript-Code-Quiz" deployedLink="https://drospond.github.io/JavaScript-Code-Quiz/"/>
           <Project title="Weather Dashboard" imageSource={WeatherDashboard} repoLink="https://github.com/drospond/Weather-Dashboard" deployedLink="https://drospond.github.io/Weather-Dashboard/"/>
         </div>
         <div className="row d-flex justify-content-center">
-          <Project title="Eat Da Burger" imageSource={Burger} repoLink="https://github.com/drospond/Eat-Da-Burger" deployedLink="https://vast-springs-71096.herokuapp.com/"/>
-          <Project title="Code Quiz" imageSource={CodeQuiz} repoLink="https://github.com/drospond/JavaScript-Code-Quiz" deployedLink="https://drospond.github.io/JavaScript-Code-Quiz/"/>
-        </div>
-        <div className="row d-flex justify-content-center">
           <Project title="Note Taker" imageSource={NoteTaker} repoLink="https://github.com/drospond/Note-Taker" deployedLink="https://apricot-pie-88422.herokuapp.com/"/>
-          <Project title="Steam Games Compare" imageSource={steamapp} repoLink="https://github.com/drospond/project-2" deployedLink="https://steam-comparison.herokuapp.com/"/>
+          <Project title="Spring Breakers" imageSource={springBreakers} repoLink="https://github.com/drospond/springBreakers/" deployedLink="https://drospond.github.io/springBreakers/"/>
         </div>
       </div>
     );
