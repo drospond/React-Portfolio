825445f1e5a437d4da2024536f898b67525ffb70
drospond <david.rospond@gmail.com>
2020-06-10 20:22:20 -0400

    added description to project card

--- client/src/Components/Project/Project.jsx
+++ client/src/Components/Project/Project.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Project.css"
 
-const Project = ({imageSource, title, repoLink,deployedLink}) => {
+const Project = ({imageSource, title, description, deployedLink}) => {
   return (
     <div className="card text-center" style={{width: "18rem"}}>
         <a href={deployedLink} target="_blank" rel="noopener noreferrer"><img src={imageSource} className="card-img-top" alt="..."/></a>
         <div className="card-body">
             <h5 className="card-title">{title}</h5>
-            <a href={repoLink} target="_blank" rel="noopener noreferrer">Repository</a>
+            <p>{description}</p>
         </div>
     </div>
   );
--- client/src/Containers/Portfolio.jsx
+++ client/src/Containers/Portfolio.jsx
@@ -18,16 +18,46 @@ class Portfolio extends Component {
           </div>
         </div>
         <div className="row d-flex justify-content-center">
-          <Project title="Steam Games Compare" imageSource={steamapp} repoLink="https://github.com/drospond/project-2" deployedLink="https://steam-comparison.herokuapp.com/"/>
-          <Project title="Friendly Fire" imageSource={friendyfire} repoLink="https://github.com/drospond/FriendlyFire" deployedLink="https://secure-tor-59417.herokuapp.com/"/>
+          <Project
+            title="Steam Games Compare"
+            imageSource={steamapp}
+            repoLink="https://github.com/drospond/project-2"
+            description="Find out what steam games you have in common with your friends."
+          />
+          <Project
+            title="Friendly Fire"
+            imageSource={friendyfire}
+            repoLink="https://github.com/drospond/FriendlyFire"
+            description="The social networking app for gamers. Find friends for the games you wnat to play."
+          />
         </div>
         <div className="row d-flex justify-content-center">
-          <Project title="Code Quiz" imageSource={CodeQuiz} repoLink="https://github.com/drospond/JavaScript-Code-Quiz" deployedLink="https://drospond.github.io/JavaScript-Code-Quiz/"/>
-          <Project title="Weather Dashboard" imageSource={WeatherDashboard} repoLink="https://github.com/drospond/Weather-Dashboard" deployedLink="https://drospond.github.io/Weather-Dashboard/"/>
+          <Project
+            title="Code Quiz"
+            imageSource={CodeQuiz}
+            repoLink="https://github.com/drospond/JavaScript-Code-Quiz"
+            description="Test your knowledge of JavaScript against the clock and try to get the high score."
+          />
+          <Project
+            title="Weather Dashboard"
+            imageSource={WeatherDashboard}
+            repoLink="https://github.com/drospond/Weather-Dashboard"
+            description="Find the weather for any city displayed in a user friendly dashboard"
+          />
         </div>
         <div className="row d-flex justify-content-center">
-          <Project title="Note Taker" imageSource={NoteTaker} repoLink="https://github.com/drospond/Note-Taker" deployedLink="https://apricot-pie-88422.herokuapp.com/"/>
-          <Project title="Spring Breakers" imageSource={springBreakers} repoLink="https://github.com/drospond/springBreakers/" deployedLink="https://drospond.github.io/springBreakers/"/>
+          <Project
+            title="Note Taker"
+            imageSource={NoteTaker}
+            repoLink="https://github.com/drospond/Note-Taker"
+            description="Create, edit, and delete notes to help you keep track of your thoughts."
+          />
+          <Project
+            title="Spring Breakers"
+            imageSource={springBreakers}
+            repoLink="https://github.com/drospond/springBreakers/"
+            description="Find cheap hotels and the best eats for your next Spring Break trip."
+          />
         </div>
       </div>
     );
