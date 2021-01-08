251893334f3ef52460bb92b3d6a041274e12c91a
drospond <david.rospond@gmail.com>
2021-01-07 19:15:49 -0500

    simplified repetetive code

--- client/src/Containers/Skills/Skills.jsx
+++ client/src/Containers/Skills/Skills.jsx
@@ -1,34 +1,46 @@
 import React, { Component } from "react";
-import './Skills.css';
+import "./Skills.css";
 
 class Skills extends Component {
   render(props) {
+    const skillList = [
+      "React",
+      "Redux",
+      "jQuery",
+      "JavaScript",
+      "MySQL",
+      "Sequelize",
+      "MongoDB",
+      "Node.js",
+      "Express",
+      "HTML",
+      "CSS",
+      "Sass",
+      "3rd party API",
+      "jsonwebtoken",
+      "bcrypt",
+      "socket.io",
+      "Wordpress",
+      "PHP",
+      "DynamoDB",
+    ];
     return (
       <div className="container white-bg border" id="Skills">
         <div className="row">
           <div className="col">
             <h3 className="border-bottom">Skills</h3>
             <p>Select a skill to view relevant projects.</p>
-            <ul className='skill-list'>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>React</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>Redux</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>jQuery</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>JavaScript</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>MySQL</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>Sequelize</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>MongoDB</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>Node.js</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>Express</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>HTML</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>CSS</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>Sass</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>3rd party API</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>jsonwebtoken</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>bcrypt</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>socket.io</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>Wordpress</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>PHP</li>
-                <li className='skill-item' onClick={(event) => this.props.filterBySkill(event)}>DynamoDB</li>
+            <ul className="skill-list">
+              {skillList.map((skill) => {
+                return (
+                  <li
+                    className="skill-item"
+                    onClick={(event) => this.props.filterBySkill(event)}
+                  >
+                    {skill}
+                  </li>
+                );
+              })}
             </ul>
           </div>
         </div>
